3d130b0634d0b1b0a970db4cd2ddf7fed67ec91b
Ivan <jei0002@outlook.com>
2025-10-25 21:04:48 -0700

    added foods page

---
+++ src/app/foods/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "@/components/PageLayout"
+
+
+export default function Foods() {
+    return (
+        <PageLayout
+            title="Foods"
+            subtitle="Be adventurous and eat at the best or worst. "
+        />
+    )
+}
