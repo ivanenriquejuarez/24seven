68f8f73170fbe64ff34f8dcb2a6cab352c78f609
Ivan <jei0002@outlook.com>
2025-10-27 02:21:45 -0700

    added food and drinks page with fake data

--- src/app/foodsanddrinks/page.tsx
+++ src/app/foodsanddrinks/page.tsx
@@ -1,11 +1,18 @@
+import FoodComponent from "@/components/FoodComponent"
 import PageLayout from "@/components/PageLayout"
-
+import { foods } from "@/data/foods"
 
 export default function FoodsAndDrinks() {
     return (
         <PageLayout
             title="Foods"
             subtitle="Be adventurous and eat at the best or worst. "
-        />
+        >
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {foods.map((food) => (
+                    <FoodComponent key={food.id} food={food}/>
+                ))}
+            </div>
+         </PageLayout>
     )
 }
