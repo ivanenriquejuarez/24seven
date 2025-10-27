9738639787d30a9fc1ac088fa44f345c892cf477
Ivan <jei0002@outlook.com>
2025-10-26 23:53:55 -0700

    added a more page

---
+++ src/app/more/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from "@/components/PageLayout";
+
+export default function More() {
+    return (
+        <PageLayout
+            title="More"
+            subtitle="Explore all the categories here..."
+            ></PageLayout>
+    )
+}
