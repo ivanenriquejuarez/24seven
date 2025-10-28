27552fd79fa8f6426cc257327e7e61bcd0733911
Ivan <jei0002@outlook.com>
2025-10-27 22:56:27 -0700

    added hotel pages

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -1,10 +1,18 @@
 import PageLayout from "@/components/PageLayout"
+import { hotels } from "@/data/hotels"
+import HotelCard from "@/components/HotelCard"
 
 export default function Hotels() {
     return (
         <PageLayout
             title="Hotels"
             subtitle="Where to stay the night?"
-        />
+        >
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {hotels.map((hotel) => (
+                    <HotelCard key={hotel.id} hotel={hotel}/>
+                ))}
+            </div>
+        </PageLayout>
     )
 }
